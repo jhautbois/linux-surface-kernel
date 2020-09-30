307eb361d479f53617e7308c5f3a5fbd5fa853b5
Jean-Michel Hautbois <jeanmichel.hautbois@gmail.com>
2020-09-30 10:01:21 +0200

    west: create yaml file to use west for the project
    The Zephyr project includes a swiss-army knife command line tool named
    west, use it for the project.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@gmail.com>

--- start_project.sh
+++ start_project.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# clone the linux repo from github
-git clone https://github.com/jhautbois/linux.git
-# clone the linux-surface repository
-git clone https://github.com/linux-surface/linux-surface.git
-# clone the linux-surface kernel configurations repository
-git clone https://github.com/linux-surface/kernel-configs
-
 systemctl -q is-active docker
 if [ $? -ne 0 ];
 then
